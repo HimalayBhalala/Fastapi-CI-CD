ff9570f2bf0b89969ed5a8782907873299e5eed9
HimalayBhalala <=>
2024-09-11 11:42:56 +0530

    Test case check

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+import os
 
 # Load environment variables from .env file
 load_dotenv()
@@ -15,9 +16,14 @@ class AllSettings(BaseSettings):
     algorithm: str
     access_token_expire_minutes: int
 
+    @classmethod
+    def __init_subclass__(cls):
+        # Convert environment variables to the expected types
+        cls.database_port = int(os.getenv("DATABASE_PORT", 5432))
+        cls.access_token_expire_minutes = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
     class Config:
         env_file = ".env"
         env_file_encoding = 'utf-8'
-
 
 settings = AllSettings()
