84272267740dfd4d2e8dc56e175b80462d206823
HimalayBhalala <=>
2024-09-11 11:46:04 +0530

    Test case check

--- app/config.py
+++ app/config.py
@@ -5,6 +5,8 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
+print("DATABASE_PORT:", os.getenv("DATABASE_PORT"))
+print("ACCESS_TOKEN_EXPIRE_MINUTES:", os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 class AllSettings(BaseSettings):
     database_username: str
@@ -16,15 +18,8 @@ class AllSettings(BaseSettings):
     algorithm: str
     access_token_expire_minutes: int
 
-    @classmethod
-    def __init_subclass__(cls):
-        # Convert environment variables to the expected types
-        cls.database_port = int(os.getenv("DATABASE_PORT", 5432))
-        cls.access_token_expire_minutes = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
-
     class Config:
         env_file = ".env"
         env_file_encoding = 'utf-8'
-
 
 settings = AllSettings()
